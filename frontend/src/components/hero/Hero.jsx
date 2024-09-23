1b0b9816b84b1916ca4cc3d9950249e8d0ef92c1
Rahulkn456 <rahulkn456@gmail.com>
2024-09-23 09:13:52 +0530

    added react fragment

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -4,14 +4,16 @@ import "./hero.css";
 const Hero = () => {
   return (
     <div className="hero-container">
-      <span className="first-title">We Are Here To Help You With</span>
-      <span className="phrase">Tax Resolutions</span>
-      <p>
-        Asset Asecenders is a hub where finacial bookeeping taxiation and
-        advisory services are systematically collated to create competitve
-        synergies
-      </p>
-      <button className="hero-btn">Schedule a call</button>
+      <>
+        <span className="first-title">We Are Here To Help You With</span>
+        <span className="phrase">Tax Resolutions</span>
+        <p>
+          Asset Asecenders is a hub where finacial bookeeping taxiation and
+          advisory services are systematically collated to create competitve
+          synergies
+        </p>
+        <button className="hero-btn">Schedule a call</button>
+      </>
     </div>
   );
 };
