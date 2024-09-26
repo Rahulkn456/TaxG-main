2a810316da632e340247d2395038efeda8f01d2e
Rahulkn456 <rahulkn456@gmail.com>
2024-09-26 16:31:30 +0530

    Added Testimonial Component

--- frontend/src/components/Testimonial/Testimonial.jsx
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -1,16 +1,71 @@
 import React from "react";
+import "./Testimonial.css";
 
-const Testimonial = ({ testimonial }) => {
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+
+import { testimonials } from "../../data";
+const Testimonial = () => {
   return (
-    <div className="testimonial">
-      {/* <img
-        src={testimonial.image}
-        alt={testimonial.name}
-        className="testimonial-image"
-      /> */}
-      <h3>{testimonial.name}</h3>
-      <p>{testimonial.message}</p>
-    </div>
+    <section className="testimonials">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-7">
+            <Swiper
+              modules={[Pagination, Autoplay]}
+              loop={true}
+              speed={600}
+              autoplay={{ delay: 5000 }}
+              slidesPerView="auto"
+              pagination={{
+                el: ".swiper-pagination",
+                type: "bullets",
+                clickable: true,
+              }}
+            >
+              <div className="swiper-wrapper">
+                {testimonials.map((testimonial) => (
+                  <SwiperSlide key={testimonial.name} className="swiper-slide">
+                    <div className="testimonial-item">
+                      <div className="d-flex">
+                        <img
+                          src={testimonial.src}
+                          alt=""
+                          className="testimonial-img flex-shrink-0"
+                        />
+                        <div>
+                          <h3>{testimonial.name}</h3>
+                          <h4>{testimonial.job}</h4>
+                          <div className="stars">
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <p>
+                        <i className="bi bi-quote right-icon"></i>
+                        <span>{testimonial.comment}</span>
+                        <i className="bi bi-quote left-icon"></i>
+                      </p>
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </div>
+              <div className="swiper-pagination">
+                <span className="swiper-pagination-bullet"></span>
+              </div>
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
---
+++ frontend/src/data.js
@@ -0,0 +1,38 @@
+import testimonial1 from "../src/images/Image1.jpg";
+import testimonial2 from "../src/images/Image2.jpg";
+import testimonial3 from "../src/images/Image3.jpg";
+import testimonial4 from "../src/images/Image4.jpg";
+import testimonial5 from "../src/images/Image5.jpg";
+
+export const testimonials = [
+  {
+    src: testimonial1,
+    name: "Paul Morgan",
+    desg: "Ceo and Founder",
+    comment: "This website is very good",
+  },
+  {
+    src: testimonial2,
+    name: "Paul Morgan",
+    desg: "Ceo and Founder",
+    comment: "This website is very good",
+  },
+  {
+    src: testimonial3,
+    name: "Paul Morgan",
+    desg: "Ceo and Founder",
+    comment: "This website is very good",
+  },
+  {
+    src: testimonial4,
+    name: "Paul Morgan",
+    desg: "Ceo and Founder",
+    comment: "This website is very good",
+  },
+  {
+    src: testimonial5,
+    name: "Paul Morgan",
+    desg: "Ceo and Founder",
+    comment: "This website is very good",
+  },
+];
